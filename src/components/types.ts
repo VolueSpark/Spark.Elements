a447b4aceb8bcf2483cde678349b2728271ff0ae
Jon-Inge Heggstad <jon.heggstad@gmail.com>
2023-04-04 11:09:05 +0200

    Add new types and mark those to be deprecated

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,3 +1,4 @@
+// TO BE DEPRECATED
 export type ChargingPlanType = {
     priceEntries: Price[]
     advice: PriceTimeRangeAdvice[]
@@ -34,5 +35,74 @@ export type ChargingPrescription = {
     q1: number
     q3: number
 }
+// END
 
-export type LegendTranslation = { [key in PriceTimeRangeAdviceType]: string }
+export type AdviceSegmentType =
+    | 'Now'
+    | 'Best'
+    | 'Good'
+    | 'Avoid'
+    | 'Worst'
+    | 'Unknown'
+
+export type ForecastBlockType = 'Normal' | 'Good' | 'Avoid'
+
+export type VAT = {
+    rate: number
+    hasVAT: boolean
+}
+
+export type PriceUnits = {
+    currency: string
+    vat: VAT
+    energyUnit: string
+}
+
+export type Segment = {
+    from: string
+    to: string
+    averagePrice: number
+}
+
+export type ForecastAdvice = Segment & {
+    type: ForecastBlockType
+    loss: number
+}
+
+export type SpotPriceAdvice = Segment & {
+    type: AdviceSegmentType
+    cost: number
+}
+
+export type SpotPrice = {
+    time: string
+    price: number
+}
+
+export type PriceRecord = Record<string, number>
+
+export type SpotPriceData = {
+    priceArea: string
+    priceUnits: PriceUnits
+    prices: PriceRecord
+}
+
+export type SpotPriceAdviceData = {
+    advice: Array<SpotPriceAdvice>
+    spotPrices: Array<SpotPrice>
+    priceArea: {
+        code: string
+    }
+    priceUnits: PriceUnits
+}
+
+export type ForecastAdviceData = {
+    priceArea: string
+    priceUnits: PriceUnits
+    segmentOptions: {
+        segmentSize: number
+    }
+    forecastAdvice: Array<ForecastAdvice>
+}
+
+export type LegendTranslation = { [key in AdviceSegmentType]?: string }
